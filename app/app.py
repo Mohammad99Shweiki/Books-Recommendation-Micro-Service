29900e2df491357cd1525d8091d439beb19c11a3
mohammadshweiki <mohammadshwiki226@gmail.com>
2024-05-09 12:16:32 +0300

    dependencies

--- app/app.py
+++ app/app.py
@@ -1,16 +1,28 @@
+from fastapi import FastAPI, HTTPException, Body
 from sentence_transformers import SentenceTransformer
-import pandas as pd
-import json
+from typing import List
+from pydantic import BaseModel
+
+# todo add authentication for one user
+app = FastAPI()
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-emb = model.encode(docs)
 
-# import model and vectorize the books descriptions and get everything done in vectors
-# check that no fine tuninig is needed
-# hit the model to check everything is alright
-# endpotins
-#   adding books vectors
-#   add user vector
-#   search by text(wether genre or title)
-# add logger in both apps
+class EmbeddingRequestBody(BaseModel):
+    content: str
+
+
+@app.post('/embed_text', response_model=List[float])
+async def embed_data(request: EmbeddingRequestBody):
+    try:
+        embedded_data = model.encode(request.content)
+        return embedded_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=9090)
